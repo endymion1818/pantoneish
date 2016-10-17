acd0d635df4a86637a456a4d8017ebe3e5a69432
endymion1818 <ben.read@indigotree.co.uk>
2016-10-17 12:39:45 +0100

    using requestAnimationFrame

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 <head>
-	<?php 
+	<?php
 	function pantonish_time_now(){
 		date_default_timezone_set('Europe/London');
 		return date('H:i:s');
@@ -19,13 +19,10 @@
 			margin: 0;
 			height: 100vh;
 		}
-		form[name="clock"] {
+		.timer {
 			position: absolute;
 			bottom: 2%;
 			right: 2%;
-		}
-		input[name="face"] {
-			border: 1px solid transparent;
 		}
 		div.clock {
 			display: flex;
@@ -38,82 +35,72 @@
 			height: auto;
 		}
 		#hours {
-			transform-origin: 50% 15%;
-			animation: rotate 60s infinite steps(60);
+			transform-origin: bottom;
 		}
 		#minutes {
-			transform-origin: 50% 70%;
-			animation: rotate 3600s infinite steps(60);
+			transform-origin: bottom;
 		}
 		#seconds {
-			transform-origin: 50% 70%;
-			animation: rotate 43200s infinite steps(60);
+			transform-origin: bottom;
 		}
 	</style>
 </head>
 <body>
 	<div class="clock">
-		<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" id="clockface" viewBox="0 0 960 960" style="enable-background:new 0 0 960 960;" xml:space="preserve">
-			<rect x="467" width="26" height="99"/>
-			<rect x="467" y="861" width="26" height="99"/>
-			<rect x="897.5" y="430.1" transform="matrix(9.162721e-04 1 -1 9.162721e-04 1389.2709 -431.3333)" width="26" height="99"/>
-			<rect x="36.5" y="430.9" transform="matrix(9.162721e-04 1 -1 9.162721e-04 529.8491 430.4541)" width="26" height="99"/>
-			<rect x="840.2" y="215.9" transform="matrix(0.4985 0.8669 -0.8669 0.4985 657.9651 -606.5336)" width="26" height="99"/>
-			<rect x="93.8" y="645.1" transform="matrix(0.4985 0.8669 -0.8669 0.4985 655.7075 255.7646)" width="26" height="99"/>
-			<rect x="839.5" y="216.3" transform="matrix(0.4985 0.8669 -0.8669 0.4985 657.9631 -605.7411)" width="26" height="99"/>
-			<rect x="93.1" y="645.5" transform="matrix(0.4985 0.8669 -0.8669 0.4985 655.7054 256.5571)" width="26" height="99"/>
-			<rect x="251.7" y="58.1" transform="matrix(0.8659 -0.5002 0.5002 0.8659 -18.3416 146.8144)" width="26" height="99"/>
-			<rect x="682.3" y="803.7" transform="matrix(0.8659 -0.5002 0.5002 0.8659 -333.5149 462.1963)" width="26" height="99"/>
-			<rect x="684.6" y="58.2" transform="matrix(0.8638 0.5038 -0.5038 0.8638 149.3005 -336.8016)" width="26" height="99"/>
-			<rect x="250.8" y="802" transform="matrix(0.8638 0.5038 -0.5038 0.8638 464.9357 -16.93)" width="26" height="99"/>
-			<polygon points="297,526 307,555 544,475 534,446 " id="hours"/>
-			<polygon points="443,432 431,446 714,710 729,695 " id="minutes"/>
-			<polygon points="487.9,101.9 480.2,102.1 472.2,648.5 482.2,649 " id="seconds"/>
-			<circle cx="480" cy="480.8" r="31.9"/>
-			<rect x="94.5" y="215.8" transform="matrix(0.5006 -0.8657 0.8657 0.5006 -175.9317 225.5804)" width="26" height="99"/>
-			<rect x="839.9" y="646.8" transform="matrix(0.5006 -0.8657 0.8657 0.5006 -176.8253 1086.0637)" width="26" height="99"/>
+		<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+			 viewBox="0 0 960 960" style="enable-background:new 0 0 960 960;" xml:space="preserve">
+			<rect x="803.7" y="252.4" transform="matrix(0.8669 -0.4985 0.4985 0.8669 -18.7346 460.6465)" width="99" height="26"/>
+			<rect x="57.3" y="681.6" transform="matrix(0.8669 -0.4985 0.4985 0.8669 -332.0417 145.6966)" width="99" height="26"/>
+			<circle cx="483.9" cy="479.6" r="31.9"/>
+			<g>
+				<rect x="470.9" width="26" height="99"/>
+				<rect x="470.9" y="861" width="26" height="99"/>
+				<rect x="864.9" y="466.6" width="99" height="26"/>
+				<rect x="0.3" y="467.4" width="99" height="26"/>
+				<rect x="806.9" y="252.8" transform="matrix(0.8669 -0.4985 0.4985 0.8669 -18.5034 462.2968)" width="99" height="26"/>
+				<rect x="60.5" y="682" transform="matrix(0.8669 -0.4985 0.4985 0.8669 -331.8105 147.347)" width="99" height="26"/>
+				<rect x="255.6" y="58.1" transform="matrix(0.8659 -0.5002 0.5002 0.8659 -17.7959 148.7821)" width="26" height="99"/>
+				<rect x="686.2" y="803.7" transform="matrix(0.8659 -0.5002 0.5002 0.8659 -333.0159 464.1476)" width="26" height="99"/>
+				<rect x="652" y="94.7" transform="matrix(0.5038 -0.8638 0.8638 0.5038 255.0425 659.4263)" width="99" height="26"/>
+				<rect x="218.2" y="838.5" transform="matrix(0.5038 -0.8638 0.8638 0.5038 -602.7146 653.7419)" width="99" height="26"/>
+				<rect x="98.4" y="215.8" transform="matrix(0.5006 -0.8657 0.8657 0.5006 -174.0726 228.9559)" width="26" height="99"/>
+				<rect x="843.8" y="646.8" transform="matrix(0.5006 -0.8657 0.8657 0.5006 -174.8501 1089.49)" width="26" height="99"/>
+			</g>
+			<rect id="hours" x="312.4" y="232.3" transform="matrix(0.457 -0.8895 0.8895 0.457 -174.1942 515.8301)" width="46" height="336.5"/>
+			<rect id="minutes" x="350" y="271.5" transform="matrix(0.3601 -0.9329 0.9329 0.3601 92.3156 703.7)" width="418.3" height="26.2"/>
+			<rect id="seconds" x="587.7" y="452.4" transform="matrix(0.8843 -0.4669 0.4669 0.8843 -251.4207 356.3238)" width="11.3" height="466.1"/>
 		</svg>
+
 	</div>
-	<form name="clock" onSubmit="0">
-		<input type="text" name="face" size=11 value ="<?= pantonish_time_now() ?>">
-	</form>
+	<div class="timer"><?= pantonish_time_now() ?></div>
 
 	<script language="JavaScript">
-	// add class to HTML tag
-	(function(H){
-		H.className=H.className.replace(/\bno-js\b/,'js')
-	})(document.documentElement);
-	// clock
-	var timerID = null
-	var timerRunning = false
+		// clock
+		 var hours = document.getElementById('hours'),
+		 	 minutes = document.getElementById('minutes'),
+		 	 seconds = document.getElementById('seconds');
 
-	function stopclock(){
-	    if(timerRunning)
-	        clearTimeout(timerID)
-	    timerRunning = false
-	}
+		window.requestAnimationFrame = window.requestAnimationFrame
+		                               || window.mozRequestAnimationFrame
+		                               || window.webkitRequestAnimationFrame
+		                               || window.msRequestAnimationFrame
+		                               || function(f){return setTimeout(f, 1000/60)}
 
-	function startclock(){
-	    stopclock()
-	    showtime()
-	}
 
-	function showtime(){
-	    var now = new Date()
-	    var hours = now.getHours()
-	    var minutes = now.getMinutes()
-	    var seconds = now.getSeconds()
-	    var timeValue = "" + ((hours > 12) ? hours - 12 : hours)
-	    timeValue  += ((minutes < 10) ? ":0" : ":") + minutes
-	    timeValue  += ((seconds < 10) ? ":0" : ":") + seconds
-	    timeValue  += (hours >= 12) ? " P.M." : " A.M."
-	    document.clock.face.value = timeValue 
-	    timerID = setTimeout("showtime()",1000)
-	    timerRunning = true
-	}
-	window.onload = function(){
-		startclock()
-	}
+		function updateclock(){
+		 var clock = new Date()
+		 var hour_as_degree = ( clock.getHours() + clock.getMinutes()/60 ) / 12 * 360
+		 var minute_as_degree = clock.getMinutes() / 60 * 360
+		 var second_as_degree = ( clock.getSeconds() + clock.getMilliseconds()/1000 ) /60 * 360
+		 hours.style.transform = 'rotate(' + hour_as_degree + 'deg)';
+		 minutes.style.transform = 'rotate(' + minute_as_degree + 'deg)';
+		 seconds.style.transform = 'rotate(' + second_as_degree + 'deg)';
+		 requestAnimationFrame(updateclock) //
+		};
+
+		requestAnimationFrame(updateclock)
+
+
 	</script>
 </body>
 </html>
