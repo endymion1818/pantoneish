2950377297bc05d4011f851f0948f9fd0bea2d42
endymion1818 <ben.read@indigotree.co.uk>
2016-11-04 13:06:21 +0000

    clock face is now accurate

--- index.php
+++ index.php
@@ -54,28 +54,31 @@
 	<div class="clock">
 		<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 			 viewBox="0 0 960 960" style="enable-background:new 0 0 960 960;" xml:space="preserve">
-			<rect x="803.7" y="252.4" transform="matrix(0.8669 -0.4985 0.4985 0.8669 -18.7346 460.6465)" width="99" height="26"/>
-			<rect x="57.3" y="681.6" transform="matrix(0.8669 -0.4985 0.4985 0.8669 -332.0417 145.6966)" width="99" height="26"/>
-			<circle cx="483.9" cy="479.6" r="31.9"/>
-			<g>
-				<rect x="470.9" width="26" height="99"/>
-				<rect x="470.9" y="861" width="26" height="99"/>
-				<rect x="864.9" y="466.6" width="99" height="26"/>
-				<rect x="0.3" y="467.4" width="99" height="26"/>
-				<rect x="806.9" y="252.8" transform="matrix(0.8669 -0.4985 0.4985 0.8669 -18.5034 462.2968)" width="99" height="26"/>
-				<rect x="60.5" y="682" transform="matrix(0.8669 -0.4985 0.4985 0.8669 -331.8105 147.347)" width="99" height="26"/>
-				<rect x="255.6" y="58.1" transform="matrix(0.8659 -0.5002 0.5002 0.8659 -17.7959 148.7821)" width="26" height="99"/>
-				<rect x="686.2" y="803.7" transform="matrix(0.8659 -0.5002 0.5002 0.8659 -333.0159 464.1476)" width="26" height="99"/>
-				<rect x="652" y="94.7" transform="matrix(0.5038 -0.8638 0.8638 0.5038 255.0425 659.4263)" width="99" height="26"/>
-				<rect x="218.2" y="838.5" transform="matrix(0.5038 -0.8638 0.8638 0.5038 -602.7146 653.7419)" width="99" height="26"/>
-				<rect x="98.4" y="215.8" transform="matrix(0.5006 -0.8657 0.8657 0.5006 -174.0726 228.9559)" width="26" height="99"/>
-				<rect x="843.8" y="646.8" transform="matrix(0.5006 -0.8657 0.8657 0.5006 -174.8501 1089.49)" width="26" height="99"/>
-			</g>
-			<rect id="hours" x="312.4" y="232.3" transform="matrix(0.457 -0.8895 0.8895 0.457 -174.1942 515.8301)" width="46" height="336.5"/>
-			<rect id="minutes" x="350" y="271.5" transform="matrix(0.3601 -0.9329 0.9329 0.3601 92.3156 703.7)" width="418.3" height="26.2"/>
-			<rect id="seconds" x="587.7" y="452.4" transform="matrix(0.8843 -0.4669 0.4669 0.8843 -251.4207 356.3238)" width="11.3" height="466.1"/>
+		<style type="text/css">
+			.st0{fill:none;}
+		</style>
+		<rect x="803.7" y="252.4" transform="matrix(0.8669 -0.4985 0.4985 0.8669 -18.7331 460.6466)" width="99" height="26"/>
+		<rect x="57.3" y="681.6" transform="matrix(0.8669 -0.4985 0.4985 0.8669 -332.0417 145.6973)" width="99" height="26"/>
+		<circle cx="480.2" cy="478.3" r="31.9"/>
+		<g>
+			<rect x="469" y="0.9" width="25.9" height="98.9"/>
+			<rect x="469" y="860.8" width="25.9" height="98.9"/>
+			<rect x="861.4" y="466.9" width="98.6" height="26"/>
+			<rect x="0.3" y="467.7" width="98.6" height="26"/>
+			<polygon points="803.8,279.8 889.2,230.5 902.1,253 816.7,302.3 	"/>
+			<polygon points="60.4,708.4 145.8,659.2 158.8,681.7 73.3,731 	"/>
+			<polygon points="231.6,72.1 254.1,59.1 303.4,144.7 281,157.7 	"/>
+			<polygon points="660.5,816.7 682.9,803.7 732.2,889.3 709.8,902.3 	"/>
+			<polygon points="662.6,144.7 712.3,59.3 734.7,72.4 685,157.8 	"/>
+			<polygon points="230.6,887.5 280.2,802.1 302.6,815.2 252.9,900.6 	"/>
+			<polygon points="61.8,252.4 74.7,229.9 160.1,279.4 147.1,301.9 	"/>
+			<polygon points="804.2,682.9 817.2,660.4 902.5,709.9 889.6,732.4 	"/>
+		</g>
+		<rect x="475.6" y="123.6" width="9" height="354.7" id="hours"/>
+		<rect x="0.3" y="0" class="st0" width="959.7" height="959.7" id="minutes"/>
+		<rect x="472.5" y="194" width="15.2" height="284.3" id="seconds"/>
+		<rect x="465.1" y="248.7" width="30.2" height="229.6" id="other"/>
 		</svg>
-
 	</div>
 	<div class="timer"><?= pantonish_time_now() ?></div>
 
@@ -100,7 +103,7 @@
 		 hours.style.transform = 'rotate(' + hour_as_degree + 'deg)';
 		 minutes.style.transform = 'rotate(' + minute_as_degree + 'deg)';
 		 seconds.style.transform = 'rotate(' + second_as_degree + 'deg)';
-		 requestAnimationFrame(updateclock) //
+		 requestAnimationFrame(updateclock) 
 		};
 
 		requestAnimationFrame(updateclock)
